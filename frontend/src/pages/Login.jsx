1c646846c38b69ffc3ee7001c8d07dd3acd1d48a
pavanfullstackdev <pavan.fullstack.dev.tech@gmail.com>
2025-01-28 21:42:30 +0530

    Functions to get the data from form are define

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -10,85 +10,126 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { useState } from "react";
 
 const Login = () => {
+  const [loginInput, setLoginInput] = useState({});
+  const [signupInput, setSignupInput] = useState({});
+
+  const changeInputHandler = (e, type) => {
+    const { name, value } = e.target; // Destructure name and value from the input event
+    if (type === "signup") {
+      setSignupInput({ ...signupInput, [name]: value });
+    } else {
+      setLoginInput({ ...loginInput, [name]: value });
+    }
+  };
+
+  const handleFormSubmission = (type) => {
+    const formData = type === "signup" ? signupInput : loginInput;
+
+    console.log(formData, "formdata");
+  };
+
+  // console.log(signupInput, "Signup Input");
+  // console.log(loginInput, "Login Input");
+
   return (
-   <div className="flex items-center w-full justify-center">
-     <Tabs defaultValue="account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="signup">Signup</TabsTrigger>
-        <TabsTrigger value="login">Login</TabsTrigger>
-      </TabsList>
-      <TabsContent value="signup">
-        <Card>
-          <CardHeader>
-            <CardTitle>Signup</CardTitle>
-            <CardDescription>
-              Create a new account and click signup when you are done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input
-                type="text"
-                placeholder="Enter your name here."
-                required="true"
-              />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                type="email"
-                placeholder="Enter your username here."
-                required="true"
-              />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                placeholder="Enter your password here."
-                required="true"
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Signup</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="login">
-        <Card>
-          <CardHeader>
-            <CardTitle>Login</CardTitle>
-            <CardDescription>Login with your credentials.</CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                type="email"
-                placeholder="Enter your username here."
-                required="true"
-              />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                type="password"
-                placeholder="Enter your password here."
-                required="true"
-              />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Login</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
-   </div>
+    <div className="flex items-center w-full justify-center">
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="signup">Signup</TabsTrigger>
+          <TabsTrigger value="login">Login</TabsTrigger>
+        </TabsList>
+        <TabsContent value="signup">
+          <Card>
+            <CardHeader>
+              <CardTitle>Signup</CardTitle>
+              <CardDescription>
+                Create a new account and click signup when you are done.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">Name</Label>
+                <Input
+                  name="name"
+                  value={signupInput.name || ""}
+                  onChange={(e) => changeInputHandler(e, "signup")}
+                  type="text"
+                  placeholder="Enter your name here."
+                  required
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name="email"
+                  value={signupInput.email || ""}
+                  onChange={(e) => changeInputHandler(e, "signup")}
+                  type="email"
+                  placeholder="Enter your email here."
+                  required
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  name="password"
+                  value={signupInput.password || ""}
+                  onChange={(e) => changeInputHandler(e, "signup")}
+                  type="password"
+                  placeholder="Enter your password here."
+                  required
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button onClick={() => handleFormSubmission("signup")}>
+                Signup
+              </Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="login">
+          <Card>
+            <CardHeader>
+              <CardTitle>Login</CardTitle>
+              <CardDescription>Login with your credentials.</CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  name="email"
+                  value={loginInput.email || ""}
+                  onChange={(e) => changeInputHandler(e, "login")}
+                  type="email"
+                  placeholder="Enter your email here."
+                  required
+                />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  name="password"
+                  value={loginInput.password || ""}
+                  onChange={(e) => changeInputHandler(e, "login")}
+                  type="password"
+                  placeholder="Enter your password here."
+                  required
+                />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button onClick={() => handleFormSubmission("login")}>
+                Login
+              </Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
   );
 };
 
